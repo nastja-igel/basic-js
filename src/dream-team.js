207de37606ca191da4d9d8e988dc8bc361d54534
Ilina <anastasia.ilina@t-systems.ru>
2020-10-06 11:04:59 +0300

    fix: dream team non-standard cases

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,6 +2,7 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
   if (Array.isArray(members) === false) return false;
-  let teamname = members.filter(i => typeof i === 'string').map(i => {return i.trim().slice(0,1);});
-  return (teamname.sort().join('').toString().toUpperCase());
+
+  let teamname = members.filter(i => typeof i === 'string').map(i => {return i.trim().slice(0,1).toUpperCase();});
+  return (teamname.sort().join(''));
 };
